27fd34228e506b4a6ff249b67e9cb82c9786daf3
i-ght <toph.ght@gmail.com>
2023-02-20 12:51:22 -0700

    Id tell you if I remember

--- Program.fs
+++ Program.fs
@@ -19,39 +19,18 @@
 ** - https://maya.nmai.si.edu/sites/all/themes/mayatime/js/calendar-converter/calendar-converter-en.js?rpyhor
 ** - https://maya.nmai.si.edu/calendar/maya-calendar-converter
 ** - https://maya.nmai.si.edu/resources
+** - https://www.sizes.com/time/cal_mayan.htm
+** - http://www.madore.org/~david/misc/calendar.html
+** - https://www.fourmilab.ch/documents/calendar/ 
 **
-** Reading the Maya Calendar
-** The Maya calendar system records a series of recurring cycles of time based on the
-** movements of the Sun, Moon, and planets. Any given date repeats at cyclic intervals, just 
-** as, for example, January 1st in the Gregorian calendar repeats every time the Earth
-** completes a revolution around the Sun. A complete Maya Long Count cycle is 5,125 years 
-** long. The Maya Long Count system establishes an absolute chronology in which any given
-** date is unique, such as December 21, 2012, in the Gregorian system. The Long Count
-** calendar keeps track of the days that have passed since the mythical starting date of the
-** Maya creation, August 11, 3114 BCE. 
+** The Long Count calendar keeps track of the days that have passed since the mythical
+** starting date of the Maya creation, August 11, 3114 BCE.
+** (September 9, 3114 BCE Julian Calendar) 
 *)
 
+open System
 
-(* credit: https://fx.sauder.ubc.ca/julian.html 
-
-function julian(Y,M,D,UT) {
-   with (Math) {
-     if (Y < 0) ++Y;
-     if (M > 2) { jy=Y;   jm=M+1;  }
-     else       { jy=Y-1; jm=M+13; }
-     jd2 = (floor(365.25*jy)+floor(30.6001*jm)+D+1720995);
-     if (D+31*(M+12*Y) >= (15+31*(10+12*1582)))
-     { ja=floor(0.01*jy);
-       jd2 += 2-ja+floor(0.25*ja);
-     }
-//
-//     jd=367*Y-floor(7*(Y+floor((M+9)/12))/4)
-//       -floor(3*(floor((Y+(M-9)/7)/100)+1)/4)
-//       +floor(275*M/9)+D+1721028.5+UT/24.0;
-   }
-   return jd2;
-}
-*)
+(* credit: https://fx.sauder.ubc.ca/julian.html *)
 
 let julianCount y m d =
     let y = if y < 0.0m then y + 1.0m else y
@@ -66,39 +45,71 @@ let julianCount y m d =
 
     let jd1 =
         if (d+31.0m*(m+12.0m*y) >= (15.0m+31.0m*(10.0m+12.0m*1582.0m))) then
-            let ja= floor(0.01m*jy)
+            let ja = floor(0.01m*jy)
             jd0 + 2.0m-ja+floor(0.25m*ja)
         else
             jd0
             
     jd1
 
+type MayaTimeCycle =
+    | Kin
+    | Uinal
+    | Tun
+    | Katun
+    | Baktun
+with
+    member cycle.DayLength =
+        match cycle with
+        | Kin -> pown 20.0m 0
+        | Uinal -> pown 20.0m 1
+        | Tun -> 18.0m * pown 20.0m 1
+        | Katun -> 18.0m * pown 20.0m 2
+        | Baktun -> 18.0m * pown 20.0m 3
+
 (*
-** credit due: 
-** - https://www.sizes.com/time/cal_mayan.htm
-** - http://www.madore.org/~david/misc/calendar.html
-**
-** Using astronomical events recorded both by Mayan and European astronomers, and historical
-** events whose dates were recorded both by Spaniards and civilizations using the Mayan 
-** calendar, scholars have sought to correlate the Mayan and Gregorian calendars. According
-** to the most widely accepted correlation (the “Goodman-Martinez-Thompson Correlation”) the
-** current Mayan epoch began on Wednesday 11 August 3113 bce (Gregorian) which is Julian date 584,282.65.
+    Thus the Maya developed a modified base 20 system in which a date is represented by a
+    number written (in modern Indo-Arabic notation) as: 
+
+    a.b.c.d.e = a(18×pown 20 3) + b(18×pown 20 2) + c(18×pown 20 1) + d(pown 20 1) + e(pown
+    20 0)
 *)
+
+let mayaLongCount y m d =
+    let julian = julianCount y m d
+
+    ()
+
 let longCount jdn =
 
     let mutable lng = [|0.0m; 0.0m; 0.0m; 0.0m; 0.0m|]
 
     let mayaConstructJdn = 584282.5m
 
+    let days = jdn - mayaConstructJdn;
+
+    (*
+        +----------+----------+---------+
+        |   Head   |   Tail   |  Days   |
+        +----------+----------+---------+
+        | 1 kin    | None     |       1 |
+        | 1 uinal  | 20 kin   |      20 |
+        | 1 tun    | 18 uinal |     360 |
+        | 1 katun  | 20 tun   |   7,200 |
+        | 1 baktun | 20 katun | 140,000 |
+        +----------+----------+---------+
+
+    *)
+
     let mutable longCount = jdn - mayaConstructJdn
 
-    lng.[0] <- floor longCount / 144000.0m
+    lng.[0] <- longCount / 144000.0m
     longCount <- longCount % 144000.0m;
     
-    lng.[1] <- floor longCount / 7200.0m
+    lng.[1] <- longCount / 7200.0m
     longCount <- longCount % 7200.0m
 
-    lng.[2] <- floor longCount / 360.0m
+    lng.[2] <- longCount / 360.0m
     longCount <- longCount % 360.0m
 
     lng.[3] <- longCount / 20.0m
@@ -111,14 +122,23 @@ let maya y m d =
     let lng = longCount <| julianCount y m d
     lng |> List.map int
 
-let now = System.DateTimeOffset.Now
+let argv = Environment.GetCommandLineArgs() 
 
-(* figure out how to convert gregorian to julian dates: https://www.fourmilab.ch/documents/calendar/*)
+let struct (y, m, d) =
+    match argv.Length with
+    | 3 ->
+        struct(
+            Decimal.Parse(argv.[0]),
+            Decimal.Parse(argv.[1]),
+            Decimal.Parse(argv.[2])
+        )
+    | _ ->
+        let now = DateTimeOffset.Now
+        ValueTuple.Create(now.Year, now.Month, now.Day)
 
-printfn "%A" <| maya -3114.0m 09.0m 6.0m
+printfn "%A" <| maya -3113.0m 9.0m 6.0m
 
-printfn "%A" 
-<| maya (decimal now.Year) (decimal now.Month) (decimal now.Day)
+printfn "%A" <| maya y m d
 
-printfn "%A" <| maya 2012.0m 12.0m 20.0m
-printfn "%A" <| maya 2012.0m 12.0m 21.0m
+
+exit 0
